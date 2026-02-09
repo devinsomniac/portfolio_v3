db400a7fbdacfd1599d5e20cf0e59b162fed51b1
devinsomniac <inzamamchowdhury20@gmail.com>
2026-02-09 10:25:57 +0000

    fea : Added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/footer"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
