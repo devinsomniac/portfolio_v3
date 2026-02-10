8c90815ebf21a68e3b9df67f57eda92967a25619
devinsomniac <inzamamchowdhury20@gmail.com>
2026-02-10 10:58:48 +0000

    feat: Sitemap and robots added

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'inzamamv3.vercel.app',
-          },
-        ],
-        destination: 'https://inzamam.cloud/:path*',
-        permanent: true,
-      },
-    ]
-  },
+  /* config options here */
 };
 
 export default nextConfig;
